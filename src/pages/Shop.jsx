5203a323933cb2ffc21dba13a43f8be5dd069b2d
amirlnsrv <amir.lansarov@gmail.com>
2025-02-03 23:14:09 +0600

    feat: added content for Shop page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Cards from "../components/Cards";
+import { womensData } from "../mock/womensData.mock";
 
 const Shop = () => {
-  return <h1>Shop</h1>;
+  return (
+    <div className="container">
+      <h1>Shop</h1>
+      <Cards cardsInfo={womensData} />
+    </div>
+  );
 };
 
 export default Shop;
